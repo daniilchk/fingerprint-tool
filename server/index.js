2c08ddd2ccd6b7bf2f8fa82389251f88c7b2406a
daniilchk <it.daniil.chk@gmail.com>
2025-05-11 00:55:42 +0300

    - [added] add cors

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { dbService } from "./services/db/index.js";
 import { router } from "./routers/index.js";
 import { errorHandler } from "./middlwares/error-handler.js";
 const PORT = process.env.PORT || 3000;
+const whitelistedDomains = process.env.WHITELISTED_DOMAINS.split(',');
 const app = express();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      const originIsWhitelisted = whitelistedDomains.indexOf(origin) !== -1;
+      callback(null, originIsWhitelisted);
+    },
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", router);
